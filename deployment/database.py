1294400dde0ff69ff3825122aee047e533a5e1a4
Sona Arzumanyan <sona.arzumanyan42@gmail.com>
2025-03-02 00:31:58 +0400

    added threading for each detection and skipping every 3rd frame to optimize the project

--- deployment/database.py
+++ deployment/database.py
@@ -6,8 +6,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///detections.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    surname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    
+    # Relationship to Detection model
+    detections = db.relationship('Detection', backref='user', lazy=True)
+
+# Detection model
 class Detection(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     emotion = db.Column(db.String(50))
     head_movement = db.Column(db.String(50))
     objects = db.Column(db.String(255))
@@ -16,3 +28,4 @@ class Detection(db.Model):
 # Create tables if they don't exist
 with app.app_context():
     db.create_all()
+    
